c8d9c10369d8f54b3baded170da1aceabd352fee
minhtri241004 <162810816+minhtri241004@users.noreply.github.com>
2026-09-07 10:58:22 +0700

    Delete checkin_face_id directory

--- checkin_face_id/frontend/src/api/systemApi.js
+++
@@ -1,7 +0,0 @@
-import { request } from './client';
-
-export const systemApi = {
-  getMyIp: () => request('/my-ip'),
-  getHealth: () => request('/health'),
-  getExportUrl: () => '/api/export'
-};
